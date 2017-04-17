9873c3a223e6be6a42042ff0fba31dd072c17db1
Jonathan Dion <jonathandionalary@gmail.com>
2017-04-17 11:34:00 -0400

    Added an example

---
+++ example/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+// import Tilt from 'react-tilt'
+import Tilt from '../../dist/tilt';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Tilt className="Tilt" style={{ height: 250, width: 250 }} >
+            <div className="Tilt-inner">
+            👽
+            </div>
+          </Tilt>
+      </div>
+    );
+  }
+}
+
+export default App;
